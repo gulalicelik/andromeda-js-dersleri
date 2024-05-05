a18540c2d73cb5ab94960a7141381bb6a9ab54de
gulalicelik <gulalicelikmi@gmail.com>
2024-05-05 17:07:22 +0300

    js examples

--- 05-05/app.js
+++ 05-05/app.js
@@ -1,8 +1,14 @@
 const myContainer = document.querySelector(".container");
+const loader  = document.querySelector(".loader");
 
+const handleDetail = (id) => {
+    localStorage.setItem("user_id",id);
+    window.location.href = "profile.html";
+}
 fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
     .then(data => {
+        loader.style.display = "none";
         data.forEach(user => {
             myContainer.innerHTML += `<div class="user-card">
         <div class="user-card__header">
@@ -12,6 +18,7 @@ fetch("https://jsonplaceholder.typicode.com/users")
             <h2 class="user-card__title">${user.name}</h2>
             <p class="user-card__description">${user.email}</p>
             <p class="">${user.phone}</p>
+            <button onclick="handleDetail(${user.id})">Detay</button>
         </div>
     </div>`;
         });
@@ -21,3 +28,8 @@ fetch("https://jsonplaceholder.typicode.com/users")
     }).finally(() => {
     console.log("Ben her zaman çalışırım");
 });
+
+// her kullanıcı kartı içerisinde bir ader buton olmalı
+// butonun adı detay olmalı
+// butona tıklandığında kullanıcının idsi ne ise localstorage içerisine
+//user_id : id şeklinde yazmalı
